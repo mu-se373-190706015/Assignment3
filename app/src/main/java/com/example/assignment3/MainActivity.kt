2e69549a1784f52bb6febf802d2cf268014ed4b4
Furkan-del <57002551+Furkan-del@users.noreply.github.com>
2022-03-21 15:32:12 +0300

    Button changed ,Command Line changed

--- app/src/main/java/com/example/assignment3/MainActivity.kt
+++ app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,11 +23,15 @@ class MainActivity : AppCompatActivity() {
             resultText.text=clicked.toString() //I GAVE TO THE BUTTON listener when you push it it reacts with this method set on clicklistener metho
             changerText.text="Button Clicked" //button clicked
         }
+        //when you push to the checkboxa checkbox is checked and then You can see the Toast message 'Push to the checkbox'
+        //and then your button title changed to the 'you can increase 'instead of button not clicked yet'
 
-check_to_initialize.setOnClickListener {
+        check_to_initialize.setOnClickListener {
     if(check_to_initialize.isChecked){
-        Toast.makeText(applicationContext,"Pushed to the Button",Toast.LENGTH_LONG).show()
+        Toast.makeText(applicationContext,"Pushed to the Checkbox",Toast.LENGTH_LONG).show()
+        button.text="You can increase"
     }
+
 }
     }
 
